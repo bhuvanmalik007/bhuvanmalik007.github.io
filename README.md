61944a3ec354f21485a9f4094773d8f2848f1ad2
bhuvan malik <bhuvanmalik007@gmail.com>
2017-07-11 03:31:14 +0530

    AUTOCOMMIT

--- README.md
+++ README.md
@@ -16,5 +16,4 @@ Run and Deployment
 Built With
 
 [![JavaScript Style Guide](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
-<img src="https://jwt.io/assets/logo.svg" width="110">
 <img src="https://redux-observable.js.org/logo/logo-small.gif" width="110">
--- src/connectors/Header.js
+++ src/connectors/Header.js
@@ -4,24 +4,52 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router' //eslint-disable-line
 import PropTypes from 'prop-types' //eslint-disable-line
 import Header from 'grommet/components/Header'
-import Title from 'grommet/components/Title'
+import styled from 'styled-components'
 import Box from 'grommet/components/Box'
+import Button from 'grommet/components/Button'
+import MailIcon from 'grommet/components/icons/base/Mail'
+import Heading from 'grommet/components/Heading'
+import Anchor from 'grommet/components/Anchor'
+import SocialMediumIcon from 'grommet/components/icons/base/SocialMedium'
+import SocialFacebookIcon from 'grommet/components/icons/base/SocialFacebook'
+import SocialGithubIcon from 'grommet/components/icons/base/SocialGithub'
+import Image from 'grommet/components/Image'
+import nameLogo from '../static/name_logo.png'
+
+const StyledAnchor = styled(Anchor)`
+  color : #000000;
+  font-size: 18px;
+  &:hover{
+    color: #865cd6
+  }
+  padding-top: 15px
+`
+
+const socialOpener = () => {
+  window.open('mailTo:bhuvanmalik1994@gmail.com', '_self')
+}
 
 export const AppHeader = props => (
-  <Header splash={false}
-    float={false}
-    fixed={false}>
-    <Title>
-      Sample Title
-    </Title>
+  <Header
+    fixed={false}
+  >
+    <Box size='xxlarge' flex align='center' alignSelf='center'
+      pad={{ horizontal: 'medium', vertical:'none' }} >
+      <Image src={nameLogo} size='large' />
+    </Box>
     <Box flex
       justify='end'
       direction='row'
-      responsive={false}>
-      icons
+      responsive={false}
+      pad={{ horizontal: 'medium', vertical:'small' }}>
+      <StyledAnchor label='PROJECTS' onClick={socialOpener} />
+      <Anchor icon={<MailIcon />} onClick={socialOpener} />
+      <Anchor icon={<SocialMediumIcon />} onClick={socialOpener} />
+      <Anchor icon={<SocialFacebookIcon />} onClick={socialOpener} />
+      <Anchor icon={<SocialGithubIcon />} onClick={socialOpener} />
     </Box>
   </Header>
-)
+      )
 
 AppHeader.propTypes = {
 }
