c1057a29f81bb9404d6aba4937115d0b5fc34edb
bhuvan malik <bhuvanmalik007@gmail.com>
2017-07-12 02:42:52 +0530

    footer layout

--- src/components/appfooter.js
+++ src/components/appfooter.js
@@ -4,18 +4,46 @@ import Footer from 'grommet/components/Footer'
 import Title from 'grommet/components/Title'
 import Box from 'grommet/components/Box'
 import Paragraph from 'grommet/components/Paragraph'
+import Image from 'grommet/components/Image'
+import skyline from '../static/skyline.png'
+import styled from 'styled-components'
+import Heading from 'grommet/components/Heading'
+
+const ContactBox = styled(Box)`
+  background: #732945;
+`
+
+const WhiteText = styled(Heading)`
+  color: #ffffff
+`
+
+const Bar = styled(Title)`
+  color: #865cd6
+`
 
 export default props =>
-  <Footer justify='between'
-    size='small'>
-    <Title>
-      Title
-    </Title>
-    <Box direction='row'
-      align='center'
-      pad={{ 'between': 'medium' }}>
-      <Paragraph margin='none'>
-        © 2017 Bhuvan Malik
-      </Paragraph>
+  <Box align='stretch' direction='column' size={{ 'width': { max: 'full' } }}
+    pad={{ between:'small' }}
+  >
+    <Box direction='row' justify='between' pad={{ horizontal: 'xlarge', vertical: 'small' }} >
+      <Image src={skyline} size='large' />
+      <ContactBox direction='column' pad='large'>
+        <WhiteText margin='none' tag='h4' >
+          CONTACT INFORMATION
+        </WhiteText>
+      </ContactBox>
     </Box>
-  </Footer>
+    <Footer justify='center' colorIndex='light-2' size='large'>
+      <Title>
+        Made with ⚛️ & ♥️
+      </Title>
+      <Bar>|</Bar>
+      <Box direction='row'
+        align='center'
+        pad={{ 'between': 'medium' }}>
+        <Paragraph margin='none'>
+          © 2017 Bhuvan Malik
+        </Paragraph>
+      </Box>
+    </Footer>
+  </Box>
--- src/core/corelayout.js
+++ src/core/corelayout.js
@@ -2,15 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Header from '../connectors/Header'
 import Footer from '../components/appfooter'
+import styled from 'styled-components'
+
+const CoreBlock = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Content = styled.div`
+  flex: 2;
+`
 
 export const CoreLayout = ({ children }) => (
-  <div>
+  <CoreBlock>
     <Header />
-    <div>
+    <Content>
       {children}
-    </div>
+    </Content>
     <Footer />
-  </div>
+  </CoreBlock>
 )
 
 CoreLayout.propTypes = {
